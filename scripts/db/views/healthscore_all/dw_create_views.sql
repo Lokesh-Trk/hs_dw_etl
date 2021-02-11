8d5cdba26f4b6ab328192f185259c0579c0cdab9
SathyaKs03 <sathya.ks@csoft-tech.com>
2021-02-11 15:23:48 +0530

    Added patient master view

---
+++ scripts/db/views/healthscore_all/dw_create_views.sql
@@ -0,0 +1,6 @@
+DROP VIEW IF EXISTS healthscore_dw.all_patient_master_view;
+CREATE VIEW healthscore_dw.all_patient_master_view as
+SELECT mph.hospital_key,dp.patient_key,patient_unique_id,patient_gender,patient_birth_dt,patient_death_ts,blood_group,contact_addr_city_nm,contact_addr_state_nm,contact_addr_country_nm,contact_addr_zipcode, date(mph.hospital_registration_ts) as first_visit_date, mph.color_category_nm as patient_category, patient_socio_economic_status,patient_occupation_status,patient_marital_status,patient_religion,patient_spouse_occupation
+ FROM healthscore_dw.dim_patient dp
+ join healthscore_dw.map_patient_hospital mph
+on mph.patient_key = dp.patient_key;
